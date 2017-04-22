a018f331a25eb75ad078a8e8159384f4cbb703d9
jnpalmstrom <jnpalmstrom@gmail.com>
2017-04-22 00:49:36 -0400

    Add files via upload

---
+++ Lab 6/stack.h
@@ -0,0 +1,24 @@
+/** File: stack.h
+ *
+ */
+
+// Name: Jack Palmstrom
+// Username: jnpalmstrom
+
+/** Struct to define a stack; each entry can hold a pointer to anything.
+ */
+struct stack {
+  void **stack_base;        // Pointer to base of stack
+  void **next;              // Pointer to next free cell;
+  int max_cells;            // Maximum number of entries in the stack
+  int cells_used;           // Currently used number of cells
+};
+typedef struct stack Stack;
+
+
+// Function prototypes
+Stack *create_stack(int max_cells);
+void delete_stack(Stack *which_stack);
+int push(Stack *which_stack, void *ptr);
+void* pop(Stack *which_stack);
+void* peek(Stack *which_stack);
